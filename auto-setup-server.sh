90f17a610cd651f0d0f53fec331b2b56fae1e39f
Kasun Harshana <kasunharshana@live.com>
2019-03-03 22:14:58 +0530

    Test

--- auto-setup-server.sh
+++ auto-setup-server.sh
@@ -5,6 +5,6 @@ if [[ "$EUID" -ne 0 ]]; then
 	exit 1
 fi
 
-echo -e "Please enter your name: "
-read name
-echo "Nice to meet you $name"
+echo -e "Please enter your vhost name: "
+read vhost_name
+echo "Nice to meet you $vhost_name"
