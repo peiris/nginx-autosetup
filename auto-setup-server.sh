91fcd6a91a64600a9d62e128ca110a08ed5706ff
Kasun Harshana <kasunharshana@live.com>
2019-03-04 00:09:23 +0530

    Update script

--- auto-setup-server.sh
+++ auto-setup-server.sh
@@ -15,8 +15,7 @@ echo "Welcome to the nginx-autoinstall script."
 echo ""
 echo "What do you want to do?"
 echo "   1) Install or update Nginx"
-echo "   2) Uninstall Nginx"
-echo "   3) Update the script"
+echo "   2) Setup VHOSTS"
 echo "   4) Exit"
 echo ""
 
@@ -45,11 +44,6 @@ case $OPTION in
 		read -n1 -r -p "Nginx is ready to be installed, press any key to continue..."
 		echo ""
 
-		# Cleanup
-		# The directory should be deleted at the end of the script, but in case it fails
-		rm -r /usr/local/src/nginx/ >> /dev/null 2>&1
-		mkdir -p /usr/local/src/nginx/modules
-
 		# Dependencies
 		apt-get update
 		apt-get install -y build-essential ca-certificates wget curl libpcre3 libpcre3-dev autoconf unzip automake libtool tar git libssl-dev zlib1g-dev uuid-dev lsb-release nginx
@@ -70,63 +64,17 @@ case $OPTION in
 		# Restart Nginx
 		systemctl restart nginx
 
-		# Removing temporary Nginx and modules files
-		rm -r /usr/local/src/nginx
-
 		# We're done !
 		echo "Installation done."
 	exit
 	;;
-	2) # Uninstall Nginx
-		while [[ $RM_CONF !=  "y" && $RM_CONF != "n" ]]; do
-			read -p "       Remove configuration files ? [y/n]: " -e RM_CONF
-		done
-		while [[ $RM_LOGS !=  "y" && $RM_LOGS != "n" ]]; do
-			read -p "       Remove logs files ? [y/n]: " -e RM_LOGS
-		done
-		# Stop Nginx
-		systemctl stop nginx
-
-		# Removing Nginx files and modules files
-		rm -r /usr/local/src/nginx \
-		/usr/sbin/nginx* \
-		/etc/logrotate.d/nginx \
-		/var/cache/nginx \
-		/lib/systemd/system/nginx.service \
-		/etc/systemd/system/multi-user.target.wants/nginx.service
-
-		# Remove conf files
-		if [[ "$RM_CONF" = 'y' ]]; then
-			rm -r /etc/nginx/
-		fi
-
-		# Remove logs
-		if [[ "$RM_LOGS" = 'y' ]]; then
-			rm -r /var/log/nginx
-		fi
-
-		# Remove Nginx APT block
-		if [[ $(lsb_release -si) == "Debian" ]] || [[ $(lsb_release -si) == "Ubuntu" ]]
-		then
-			rm /etc/apt/preferences.d/nginx-block
-		fi
-
-		# We're done !
-		echo "Uninstallation done."
-
-	exit
-	;;
-	3) # Update the script
-		wget https://raw.githubusercontent.com/peiris/nginx-autosetup/master/auto-setup-server.sh -O auto-setup-server.sh
-		chmod +x auto-setup-server.sh
+	2) # Setup VHOSTS
 		echo ""
-		echo "Update done."
-		sleep 2
-		./auto-setup-server.sh
-		exit
-	;;
-	4) # Exit
-		exit
+		echo "What is the domain name?"
+		echo ""
+		while [[ $NEW_VHOST_NAME !=  "y" && $NEW_VHOST_NAME != "n" ]]; do
+			read -p "Please enter domain name" -e NEW_VHOST_NAME
+		done
 	;;
 
 esac
